49d7bacb04967dba6decb38857303fa60ad4f7ed
bgsrio <bgsrio@gmail.com>
2022-11-28 22:45:19 -0300

    get e post ok

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express, { response } from "express";
 import * as dotenv from "dotenv";
+import { v4 as uuidv4 } from "uuid";
 
 //habilitar o servidor a ter variáveis de ambiente
 dotenv.config();
 
 //instanciar a variável responsável pelo servidor
 const app = express();
-//configurar o servidorpara aceritar enviar e receber arquivos em JSON
+//configurar o servidor para aceritar enviar e receber arquivos em JSON
 app.use(express.json());
 
-const data = [
+let data = [
   {
     name: "Ana",
     age: 30,
@@ -22,6 +23,21 @@ app.get("/", (request, response) => {
   //retorna uam resposta com status de 200 e um json
   return response.status(200).json(data);
 });
+
+//MÉTODO POST
+app.post('/create', (request, response) => {
+  const newData = {
+    //captura o body da requisição e adiciona um id
+    ...request.body,
+    id: uuidv4(),
+  };
+  data.push(newData);
+
+  return response.status(201).json(data);
+});
+
+//método put
+
 
 // servidor subindo pro ar
 app.listen(Number(process.env.PORT), () => {
